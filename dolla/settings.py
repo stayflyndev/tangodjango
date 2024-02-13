d4c3d3c606738aa22828f9a067e746d2429429c8
stayflyndev <aretee64@gmail.com>
2024-02-13 18:51:09 -0500

    adding start strap bootstrap template

--- dolla/settings.py
+++ dolla/settings.py
@@ -109,8 +109,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "static/"
+STATICFILES_DIRS = ['static/']
 MEDIA_URL = 'root/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
